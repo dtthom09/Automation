25f25eba251122555d44be135f05c6422097188e
Dylan T Thomas <dtthom09@cardmail.louisville.edu>
2020-11-04 15:59:10 -0500

    files are moving to new folder

--- Files.py
+++ Files.py
@@ -10,18 +10,17 @@ class MyHandler(FileSystemEventHandler):
 
     def on_modified(self, event):
         for filename in os.listdir(folder_to_track):
-            src = folder_to_track + "/" + filename
-            new_destination = folder_destination + "/" + filename
+            src = folder_to_track + '\\' + filename
+            new_destination = folder_destination + r'\\' + filename
             os.rename(src, new_destination)
 
 
-folder_to_track = ''
-folder_destination = ''
+folder_to_track = r'C:\Users\Dylan\Downloads'
+folder_destination = r'C:\Users\Dylan\Downloads2'
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler, folder_to_track, recursive=True)
 observer.start()
-
 
 try:
     while True:
